25070480ef832bed27bb242b01d2aaa9eac7e8dc
David Saper <davidsaper2@gmail.com>
2020-04-04 15:50:36 +0300

    added date

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import logo from './oknews.png';
 
-const AppHeader = () => (
-	<div className="app-header">
-		<img src={logo} className="logo" alt="logo" />
-		<div className="desc">כל החדשות המעניינות והרלוונטיות מהאתרים המובילים בישראל. </div>
-	</div>
-);
+const DAYS = {
+	0: ' ראשון',
+	1: ' שני',
+	2: ' שלישי',
+	3: ' רביעי',
+	4: ' חמישי',
+	5: ' שישי',
+	6: ' שבת',
+};
+const AppHeader = () => {
+	const now = new Date();
+	return (
+		<div className="app-header">
+			<div className="date">
+				יום
+				{`${DAYS[now.getDay()]}`}
+			</div>
+			<img src={logo} className="logo" alt="logo" />
+			<div className="desc">כל החדשות המעניינות והרלוונטיות מהאתרים המובילים בישראל.</div>
+		</div>
+	);
+}
 
 export default AppHeader;
